abae98b4f44d719f8ab4be6435f666ab3f18489d
Rafael Ribeiro Estrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2024-03-09 09:11:37 -0400

    azure active directory base

---
+++ spring-cloud-azure/azure-active-directory/src/main/java/com/example/azureactivedirectory/AzureActiveDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.azureactivedirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureActiveDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureActiveDirectoryApplication.class, args);
+	}
+
+}
